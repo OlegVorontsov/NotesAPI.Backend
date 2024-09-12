2ffa134fcf373083ffceeb291dbfaef63825dfea
Олег Воронцов <oleg-voroncov@mail.ru>
2024-09-12 15:02:33 +0300

    db context, db initializer, automapper added

---
+++ Notes.Application/Common/Mappings/AssymblyMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Notes.Application.Common.Mappings
+{
+    public class AssymblyMappingProfile : Profile
+    {
+        // assymbly - ссылка на сборку
+        public AssymblyMappingProfile(Assembly assymbly) =>
+            ApplyMappingsFromAssembly(assymbly);
+
+        private void ApplyMappingsFromAssembly(Assembly assymbly)
+        {
+            var types = assymbly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                .Any(i => i.IsGenericType &&
+                i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+
+    }
+}
---
+++ Notes.Rersistance/NotesDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Rersistance.EntityTypeConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Rersistance
+{
+    public class NotesDbContext : DbContext, INotesDBContext
+    {
+        public DbSet<Note> Notes { get ; set ; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options)
+        {}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
