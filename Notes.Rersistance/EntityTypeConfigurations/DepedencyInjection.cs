a4b858dcfd6411d0a06d57b5bc652e791b679d87
Олег Воронцов <oleg-voroncov@mail.ru>
2024-09-13 15:51:06 +0300

    DependencyInjections added

---
+++ Notes.Rersistance/EntityTypeConfigurations/DepedencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Rersistance.EntityTypeConfigurations
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectiongString = configuration["DbConnection"];
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlite(connectiongString);
+            });
+            services.AddScoped<INotesDbContext>(provider =>
+                provider.GetService<NotesDbContext>());
+            return services;
+        }
+    }
+}
